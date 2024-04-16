1e72c451d445d56b5f0f5a165c96e0f40432154f
sunsetheus <williamsalimrjs@gmail.com>
2024-04-16 21:02:09 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/subscription/SubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
