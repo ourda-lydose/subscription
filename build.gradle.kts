d4915bc3b50246efc0e3026b1efb12741ce4a48b
stronovski <andisalsabilaardian@gmail.com>
2024-05-30 18:15:54 +0700

    add jacoco (i forgot)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 val junitJupiterVersion = "5.9.1"
 
 plugins {
-	java
 	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.8.10"
+	kotlin("plugin.spring") version "1.8.10"
+	id("jacoco")
+	id("org.sonarqube") version "3.5.0.2730"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -29,13 +32,16 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
 	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -52,4 +58,12 @@ tasks.register<Test>("unitTest") {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+	reports {
+		xml.required.set(true)
+		csv.required.set(false)
+		html.required.set(true)
+	}
 }
